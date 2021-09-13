241226f033ad01ac8ae56949c9e65375a3a2ed96
Harold Kim <root@stypr.com>
2021-09-13 07:49:22 -0700

    Add VPN services

---
+++ plugins/vultr.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python -u
+# -*- coding: utf-8 -*-
+"""
+vultr.py
+
+Helper class for vultr
+"""
+from .vendor import vultr
+
+def list_server():
+    """
+    List instances in json
+
+    {"instances": [{...}, {...}, ...]}
+    """
+    return vultr.list_instances()
+
+def delete_server(instance_name):
+    """ (str) -> bool
+
+    Delete server
+    """
+    for _instance in list_server()['instances']:
+        if _instance['label'] == instance_name:
+            vultr.delete_instance(instance_name)
+            return True
+    return False
+
+def add_server(name, snapshot_id):
+    """ (str, str) ->json()
+    Add server based on given snapshot
+    """
+    return vultr.create_instance(name, snapshot_id)
