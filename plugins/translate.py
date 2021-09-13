1db9a104208baf965c027ab2c0bbbff0746f391e
Harold Kim <root@stypr.com>
2021-09-13 00:56:58 -0700

    Initial Commit

---
+++ plugins/translate.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python -u
+# -*- coding: utf-8 -*-
+"""
+translate.py
+
+Translate Format
+"""
+from .vendor import papago
+
+def translate_text(self, author, channel_name, message):
+    """ (Translate, str, str, str) -> str
+
+    Translate text and output
+    """
+    current_lang = papago.check_language(message)
+    result = {}
+
+    if current_lang == "Japanese":
+        result[':flag_jp:'] = message
+        result[':flag_kr:'] = papago.Papago.translate_text("ja", "ko", message)
+        result[':flag_us:'] = papago.Papago.translate_text("ja", "en", message)
+    elif current_lang == "English":
+        result[':flag_us:'] = message
+        result[':flag_kr:'] = papago.Papago.translate_text("en", "ko", message)
+        result[':flag_jp:'] = papago.Papago.translate_text("en", "ja", message)
+    elif current_lang == "Korean":
+        result[':flag_kr:'] = message
+        result[':flag_us:'] = papago.Papago.translate_text("ko", "en", message)
+        result[':flag_jp:'] = papago.Papago.translate_text("ko", "ja", message)
+
+    output = f"**@{author[0] + '·' + author[1:]}** from **#{channel_name}** said\n"
+    for _key in result:
+        result[_key] = result[_key].strip()
+        if result[_key] == "":
+            return ""
+        output += f"> {_key}: {result[_key]}\n"
+
+    return output
