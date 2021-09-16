5f854524467079b2beb218d8d5510ca1edfc4594
Harold Kim <root@stypr.com>
2021-09-16 08:28:35 -0700

    Add hashcrack

---
+++ plugins/hashcrack.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python -u
+# -*- coding: utf-8 -*-
+"""
+translate.py
+
+Translate Format
+"""
+from .vendor.hashcrack import crack_hash
+from collections import Counter
+
+def crack_text(message):
+    """ (str) -> str
+
+    Crack text
+    """
+    output = ""
+    result = crack_hash(message)
+
+    if result:
+        # Find most common string
+        _count = Counter(result)
+        output = f":green_heart: Hash cracked! Decrypted as **{_count.most_common(1)[0][0]}**"
+    else:
+        output = ":warning: Failed to crack hash!"
+
+    return output
